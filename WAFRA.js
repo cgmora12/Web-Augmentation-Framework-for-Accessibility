0d52a1b14bbd8b5658d5ef59db8ba12c59f234c7
cgmora12 <cgmora12@hotmail.com>
2020-01-21 12:15:40 +0100

    Update version code

--- WAFRA.js
+++ WAFRA.js
@@ -3,7 +3,7 @@
 // @updateURL    https://raw.githubusercontent.com/cgmora12/Web-Augmentation-Framework-for-Accessibility/master/WAFRA.js
 // @downloadURL  https://raw.githubusercontent.com/cgmora12/Web-Augmentation-Framework-for-Accessibility/master/WAFRA.js
 // @namespace    http://tampermonkey.net/
-// @version      0.92
+// @version      0.93
 // @description  Web Augmentation Framework for Accessibility (WAFRA)
 // @author       Cesar Gonzalez Mora
 // @match        *://*/*
